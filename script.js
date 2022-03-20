960ff07233e806bbd808b8fbebef9094a9bf75ba
Rinasham <rinashamau@gmail.com>
2022-03-20 18:13:21 +1100

    switch 3 courses

--- script.js
+++ script.js
@@ -10,20 +10,44 @@ const guessesArr = [
 // Boolean 'state' controls the acceptance of keypress
 let state = false
 
+// Variable for counting HOW MANY TIMES you answered
+let answerCount = 0
+
+
+// start buttons variable
+const startBtns = document.getElementsByClassName('startBtn')
+const InputStartBtn = document.getElementById('InputStartBtn')
+const onscreenStartBtn = document.getElementById('OnscreenStartBtn')
+const keyboardStartBtn = document.getElementById('KeyboardStartBtn')
+
+
 // main
-document.getElementById('startBtn').addEventListener('click', function(){
-  startGame()
-})
+for (let startBtn of startBtns){
+  startBtn.addEventListener('click', function(e){
+    const whichBtn = e.target.id
+    console.log(whichBtn)
+    // parameter == which button was pressed to start
+    startGame(whichBtn)
+  })
+}
+
 
 // START GAME
-function startGame(){
+function startGame(startBtn){
   // change screen
   // 後でコメントアウト　↓
-  // document.getElementById('startArea').style.display = 'none'
-  // document.getElementById('gameArea').style.display = 'block'
+  document.getElementById('gameArea').style.display = 'block'
+  document.getElementById('startArea').style.display = 'none'
+  if (startBtn === 'InputStartBtn'){
+    document.getElementById('InputGameArea').style.display = 'block'
+  } else if (startBtn === 'OnscreenStartBtn'){
+    document.getElementById('onscreenArea').style.display = 'block'
+  } else {
+    document.querySelector('h3').style.display = 'block'
+  }
 
+  // accept keypress
   state = !state
-
 }
 
 
@@ -31,21 +55,36 @@ function startGame(){
 // FOR INPUT EVENT
 document.getElementById('answerBtn').addEventListener('click', function(){
   let answerInputArea = document.querySelector('input')
-  // if the value in the input box == 5, 
+  // if the value in the input box == 5,
+  // put the answer into guessArr
+  // add 1 to answerCount variable
   if (answerInputArea.value.length !== 5){
     ;
   } else {
     console.log('ok')
   }
+  // when answerCount variable gets up to 6, function finish fires
 })
+
+
+
 
 // FOR ONSCREEN KEYBOARD INPUT
 const keys = document.getElementsByClassName('keys')
 for (let key of keys){
   key.addEventListener('click', function(){
-    console.log(key.dataset['letter'])
+    if (key.dataset['letter'] === 'enter'){
+      console.log('ENTER dayooooo')
+    } else if (key.dataset['letter'] === 'backspace'){
+      console.log('BACKSPACE dayooooo')
+    } else {
+      // 文字がアルファベットだった時の処理
+    }
   })
 }
+
+
+
 
 
 // FOR KEYPRESS EVENT
