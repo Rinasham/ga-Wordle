c21901378275aa98ed3cb573bc6911acf00f6d6d
Rinasham <rinashamau@gmail.com>
2022-03-21 22:36:53 +1100

    [fix] keyboard

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ console.log(`Question is ${question}`) // 後でコメントアウト
 
 // start buttons variable
 const startBtns = document.getElementsByClassName('startBtn')
-const InputStartBtn = document.getElementById('InputStartBtn')
 const onscreenStartBtn = document.getElementById('OnscreenStartBtn')
 const keyboardStartBtn = document.getElementById('KeyboardStartBtn')
 
@@ -39,10 +38,7 @@ function startGame(startBtn){
   document.getElementById('startArea').style.display = 'none'
   // accept keypress
   state = !state
-  if (startBtn === 'InputStartBtn'){
-    document.getElementById('InputGameArea').style.display = 'block'
-    inputGame()
-  } else if (startBtn === 'OnscreenStartBtn'){
+  if (startBtn === 'OnscreenStartBtn'){
     document.getElementById('onscreenArea').style.display = 'block'
   } else {
     document.querySelector('h3').style.display = 'block'
@@ -50,75 +46,11 @@ function startGame(startBtn){
   }
 }
 
-let guessedLetters = ''
+// let guessedLetters = ''
 
 // 5 boxes in a row
   // the number of a row is `row${answerCount + 1}`
   let boxesInRow = document.getElementById(`row${answerCount + 1}`).children
-
-// FOR INPUT EVENT
-function inputGame(){
-  if(!state) return
-
-  let inputBox = document.querySelector('input')
-
-  inputBox.addEventListener('keyup', function(e){
-    if (!state) return
-    let boxesInRow = document.getElementById(`row${answerCount + 1}`).children
-    guessedLetters = e.target.value //input formに入っているリアルタイムの文字列、 delete処理いらない
-    // sync the input form value with the letters in cells
-    if(e.key === 'Backspace'){
-      // if the input form value's length is less than 5,
-      // and the event key is 'backSpace',
-      // delete matching letter in the cells
-      if (0 < guessedLetters.length < 5){
-        console.log(boxesInRow)
-        console.log(guessedLetters)
-        boxesInRow[howManyLetters -1].textContent = ''
-        howManyLetters -= 1
-        guessesArr[answerCount] = guessesArr[answerCount].slice(0, -1)
-        console.log(guessesArr)
-      }
-      // show typed letters in the cells
-      //if pressed any of meta keys(?)
-    } else if (!e.code.startsWith('Key')){
-      ;
-      // if the key is something able to input in the input box
-      //(to get it consistent with letter index)
-    } else if (e.code == /(Period|Slash)/){
-      console.log('ダメー')
-    }else {
-      if(howManyLetters < 5) {
-        // console.log(boxesInRow)
-        boxesInRow[howManyLetters].textContent = guessedLetters[howManyLetters]
-        howManyLetters += 1
-        guessesArr[answerCount] += e.key
-        // console.log(guessesArr)
-      }
-    }
-  })
-
-
-
-  document.getElementById('answerBtn').addEventListener('click', function(){
-    // reset the letter index in rows so that you can move on to the next guess
-    howManyLetters = 0
-
-    let answerInputArea = document.querySelector('input')
-
-    // clear the input box automatically so as not to make the same answer by accident
-      answerInputArea.value = ''
-    // check the guess, parameter is the user's guess
-      checkAnswer(guessesArr[answerCount], question)
-    // how many times has the user guessed? + 1
-      answerCount += 1
-    // // when answerCount variable gets up to 6, function finish fires
-    if (answerCount === 6){
-      finish()
-      state = !state
-    }
-  })
-}
 
 
 
@@ -200,7 +132,6 @@ function createQuestion(){
 // CHECK THE GUESS
 // parameter == user's guess (strings)
 function checkAnswer(guess){
-  console.log(guess)
   // 5 boxes in a row
   // the number of a row is `row${answerCount + 1}`
   let boxesInRow = document.getElementById(`row${answerCount + 1}`).children
@@ -209,14 +140,12 @@ function checkAnswer(guess){
   for (let i=0; i<guess.length; i++){
     // Is that letter included?
     if(!question.includes(guess[i])){
-      // console.log(boxesInRow[i].textContent)
       boxesInRow[i].style.backgroundColor = 'grey'
       boxesInRow[i].style.color = 'white'
     } else {
       // Is position correct?
-      const indexOfLetterQuestion = question.indexOf(boxesInRow[i].textContent)
-      // console.log(i)
-      if (indexOfLetterQuestion === i){
+      console.log(question)
+      if (question[i] === boxesInRow[i].textContent){
         boxesInRow[i].style.backgroundColor = 'green'
         boxesInRow[i].style.color = 'white'
         boxesInRow[i].classList.add('green')
@@ -226,8 +155,9 @@ function checkAnswer(guess){
       }
     }
     let correctAnswersNum = document.getElementsByClassName('green')
-    if (correctAnswersNum.length === 6){
-      console.log('6個正解だよ')
+    if (correctAnswersNum.length === 5){
+      console.log('正解だよ')
+      console.log(`${answerCount + 1}ターンで正解`)
     }
   }
 }
