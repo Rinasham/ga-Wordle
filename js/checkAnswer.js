8ee29ab6ae05c711fb7201421a6fbe4e696bdf2f
Rinasham <rinashamau@gmail.com>
2022-03-25 02:33:22 +1100

    [fix] duplicate

--- js/checkAnswer.js
+++ js/checkAnswer.js
@@ -6,19 +6,20 @@
 
 
 function checkAnswer(guess){
-
-
   // validation 後でコメントアウト戻す
-//   console.log(guess.toUpperCase())
-//   if (validWordsSet.has(guess.toUpperCase())){
-//     console.log('そのワードあるよ')
-//     checkLetters(guess)
-//   } else {
-//     // 画面にそんなワードは有効じゃないと表示させる
-//     console.log('そのワードはありません')
-//   }
+  console.log(guess.toUpperCase())
+  if (validWordsSet.has(guess.toUpperCase())){
+    console.log('そのワードあるよ')
+    checkLetters(guess)
+  } else {
+    // 画面にそんなワードは有効じゃないと表示させる
+    console.log('そのワードはありません')
+  }
 checkLetters(guess)
 }
+
+const correctLetters = []
+const correctKeys = []
 
 // guess == aguessesArr[answerCount]
 function checkLetters(guess){
@@ -27,10 +28,8 @@ function checkLetters(guess){
   let boxesInRow = document.getElementById(`row${answerCount + 1}`).children
   // get all keys in onscreen keyboard
   const screenKeys = document.getElementsByClassName('keys')
-  console.log(guess) //capital
-  console.log(screenKeys[0].textContent) // capital
 
-  const correctLetters = []
+
   for (let letter of question){
     correctLetters.push(letter)
   }
@@ -39,80 +38,74 @@ function checkLetters(guess){
   // guessを一文字ずつcheck
   function checkGreen(guess){
     for (let i=0; i<guess.length; i++){
-      // Is position correct?
+      if(question[i] === guess[i]){
+        correctKeys.push(guess[i])
+      }
+    }
+  }
+
+  // this Set contains characters in answer
+  // but can have less characters than 5
+  const correctLettersSet = new Set([...correctLetters])
+
+
+  for (let i=0; i<guess.length; i++){
+    checkGreen(guess)
+
+// 正解の中に含まれているパターン
+    if (correctLettersSet.has(guess[i])){
       if (question[i] === guess[i]){
+        // ボックス緑
         boxesInRow[i].style.backgroundColor = 'rgba(0,128,0,0.75)'
         boxesInRow[i].style.color = 'white'
         boxesInRow[i].classList.add('green')
-
         correctLetters.splice(i, 1, '')
-
-        console.log(correctLetters)
-
-        console.log(boxesInRow[i].textContent, guess[i])
+        // キーも緑
         for (let key of screenKeys){
-          // console.log(key.textContent)
           if(key.textContent === guess[i]){
             key.style.backgroundColor = 'rgba(0,128,0,0.75)'
             key.style.color = 'white'
             key.classList.add('greenKey')
+            console.log('グリーーーーーん')
+          }
+        }
+        // 正解arrの中に含まれている、けど順番が違う
+      } else {
+        // ボックスは黄色
+        boxesInRow[i].style.backgroundColor = 'rgba(241,202,43,0.85)'
+        boxesInRow[i].style.color = 'white'
+        for (let key of screenKeys){
+          if(key.textContent === guess[i]){
+            if(key.classList.contains('greenKey')){
+              ;
+            }else{
+              key.style.backgroundColor = 'rgba(241,202,43,0.85)'
+              key.style.color = 'white'
+            }
           }
         }
       }
-    }
-  }
-
-  checkGreen(guess)
-
-  const correctLettersSet = new Set([...correctLetters])////////
-  console.log(correctLettersSet)
-
-  for (let i=0; i<guess.length; i++){
-    if (boxesInRow[i].classList.contains('green')){
-      ;
-    } else if (!correctLettersSet.has(guess[i])){
+      //正解の中に含まれていないパターン
+    } else {
         boxesInRow[i].style.backgroundColor = 'rgba(128,128,128,0.85)'
         boxesInRow[i].style.color = 'white'
         for (let key of screenKeys){
-          if (key.textContent == guess[i]){
+          if(key.textContent === guess[i]){
             key.style.backgroundColor = 'rgba(128,128,128,0.85)'
             key.style.color = 'white'
           }
         }
-      } else {
-        boxesInRow[i].style.backgroundColor = 'rgba(241,202,43,0.85)'
-        boxesInRow[i].style.color = 'white'
-        for (let key of screenKeys){
-          if (key.textContent == guess[i]){
-            key.style.backgroundColor = 'rgba(241,202,43,0.85)'
-            key.style.color = 'white'
-          }
-        }
-      }
-// console.log(correctLettersSet)
-//     for (let key of screenKeys){
-//       if(key.classList.contains('greenKey')){
-//         ;
-//       }else{
-//         if (!correctLettersSet.has(guess[i])){
-//           key.style.backgroundColor = 'rgba(128,128,128,0.85)'
-//           key.style.color = 'white'
-//         } else {
-//           key.style.backgroundColor = 'rgba(241,202,43,0.85)'
-//           key.style.color = 'white'
-//         }
-//       }
-//     }
+    }
   }
 
-  let correctAnswersNum = document.getElementsByClassName('green')
+
+  let targetRow = document.getElementById(`row${answerCount + 1}`)
+  console.log(answerCount)
+  console.log(targetRow)
+  let correctAnswersNum = targetRow.getElementsByClassName('green')
   if (correctAnswersNum.length === 5){
     timerFlag = !timerFlag
     prevStreak += 1
     finish('win')
-  } else {
-    for(let i=0; i<guess.length; i++){
-      boxesInRow[i].classList.remove('green')
-    }
   }
 }
